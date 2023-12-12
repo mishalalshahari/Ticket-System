dd7df3d9294a242b264907916c23b70ea43dae94
mishalalshahari <mishalalshahari@gmail.com>
2023-12-12 19:34:59 +0530

    added layout

---
+++ frontend/src/layout/DefaultLayout.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Header } from "./partials/Header.component";
+import { Footer } from "./partials/Footer.component";
+
+export const DefaultLayout = ({ children }) => {
+  return (
+    <div>
+      <div className="default-layout">
+        <header className="header">
+          <Header />
+        </header>
+        <main className="main">{children}</main>
+        <footer className="footer">
+          <Footer />
+        </footer>
+      </div>
+    </div>
+  );
+};
---
+++ frontend/src/layout/partials/Footer.component.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="text-center copy-right">
+      &copy; 2023 All rights reserved. Powered by Mishal Al Shahari
+    </div>
+  );
+};
